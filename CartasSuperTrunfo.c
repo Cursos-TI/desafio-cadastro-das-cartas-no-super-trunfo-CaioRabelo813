3a80de2787401daeef1c18886f4091e6a8d5f44a
CaioRabelo813 <caioravbeloc47@gmail.com>
2025-04-08 05:52:42 +0000

    Nível Novato: Cadastro Básico

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,14 +7,12 @@ struct carta {
     char cidade[50];
     int populacao;
     float area;
-    double pib;
+    float pib;
     int pontosturisticos;
 };
 
 int main() {
     struct carta carta1, carta2;
-
-    printf("Desafio Super Trunfo - Países\n\n");
 
     // Carta 1
     printf("Cadastro da primeira carta:\n");
@@ -29,7 +27,7 @@ int main() {
     printf("Área (em km²): ");
     scanf("%f", &carta1.area);
     printf("PIB: ");
-    scanf("%lf", &carta1.pib);
+    scanf("%f", &carta1.pib);
     printf("Número de pontos turísticos: ");
     scanf("%d", &carta1.pontosturisticos);
 
@@ -48,7 +46,7 @@ int main() {
     printf("Área (em km²): ");
     scanf("%f", &carta2.area);
     printf("PIB: ");
-    scanf("%lf", &carta2.pib);
+    scanf("%f", &carta2.pib);
     printf("Número de pontos turísticos: ");
     scanf("%d", &carta2.pontosturisticos);
 
@@ -58,7 +56,7 @@ int main() {
     printf("Cidade: %s\n", carta1.cidade);
     printf("População: %d\n", carta1.populacao);
     printf("Área: %.2f km²\n", carta1.area);
-    printf("PIB: %.2lf\n", carta1.pib);
+    printf("PIB: %.2f\n", carta1.pib);
     printf("Pontos turísticos: %d\n", carta1.pontosturisticos);
 
     printf("\n--- Carta 2 ---\n");
@@ -67,7 +65,7 @@ int main() {
     printf("Cidade: %s\n", carta2.cidade);
     printf("População: %d\n", carta2.populacao);
     printf("Área: %.2f km²\n", carta2.area);
-    printf("PIB: %.2lf\n", carta2.pib);
+    printf("PIB: %.2f\n", carta2.pib);
     printf("Pontos turísticos: %d\n", carta2.pontosturisticos);
 
     return 0;
